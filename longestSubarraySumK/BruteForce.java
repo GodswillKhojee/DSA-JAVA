5da141366da2d3a7eb2f87bcbb0191c46147f141
Gods Will Khojee <ardiumwing@gmail.com>
2026-03-30 01:27:13 +0530

    longestSubarrayBrute

---
+++ longestSubarraySumK/BruteForce.java
@@ -0,0 +1,26 @@
+package longestSubarraySumK;
+
+import java.util.*;
+public class BruteForce {
+    public static int LongestSubarray(int [] nums, int k)
+    {
+        int n = nums.length;
+        int maxl = 0;
+        for(int s = 0; s<n;s++)
+        {
+            int curSum = 0;
+            for(int e = s; e < n; e++)
+            {
+                curSum += nums[e];
+                if (curSum == k) maxl = Math.max(maxl, e-s+1);
+            }
+        }
+        return maxl;
+    }
+    public static void main(String[] args) {
+        int [] nums = {-3, 2, 1};
+        int k = 6;
+        int len = LongestSubarray(nums,k);
+        System.out.println(len);
+    }
+}
