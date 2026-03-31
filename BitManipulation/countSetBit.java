eb3d9298ec76f2936dde52e211281ac9144cbfa3
Gods Will Khojee <ardiumwing@gmail.com>
2026-04-01 04:04:27 +0530

    countBit

--- BitManipulation/countSetBit.java
+++ BitManipulation/countSetBit.java
@@ -1,8 +1,48 @@
 package BitManipulation;
 
 public class countSetBit {
+    public static  int builtin(int n)
+    {
+        // builtin method
+        return Integer.bitCount(n);
+    }
+    public static  int raw(int n )
+    {
+        int cnt = 0;
+        while(n > 1)
+        {
+            if(n%2 == 1) cnt++;
+            n /= 2;
+        }
+        if(n==1)cnt++;
+        return cnt;
+    }
+    public static int bitRaw(int n)
+    {
+        int cnt = 0;
+        while(n>1)
+        {
+            cnt += n&1;
+            n=n>>1;
+        }
+        if(n==1)cnt++;
+        return cnt;
+    }
+    public static int UsingLastBit(int n )
+    {
+        int cnt = 0;
+        while(n!=0)
+        {
+            n = n & (n-1);
+            cnt++;
+        }
+        return  cnt;
+    }
     public static void main(String[] args) {
-        // built-in function
-        System.out.println(built-in);
+        int n = 13;
+        System.out.println(builtin(n));
+        System.out.println(raw(n));
+        System.out.println(bitRaw(n));
+        System.out.println(UsingLastBit(n));
     }
 }
