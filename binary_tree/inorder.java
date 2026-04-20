34a76b64ea91d7c7e2e13501afb6bfc02e9ba703
Gods Will Khojee <ardiumwing@gmail.com>
2026-04-20 19:24:51 +0530

    inorder traversal

--- binary_tree/inorder.java
+++ binary_tree/inorder.java
@@ -1,19 +1,19 @@
 package binary_tree;
 
-public class preorderTranversal {
+
+public class inorder {
     public static void main(String[] args) {
         node root = new node(1);
         root.left = new node(2);
         root.right = new node(3);
         root.right.left = new node(5);
-        preorder(root); // this is the preorder recursive method
-
+        InorderTranversal(root);
     }
-    public static void preorder(node root)
+    public static void InorderTranversal(node root)
     {
         if(root == null) return;
-        System.out.print(root.data + "->");
-        preorder(root.left);
-        preorder(root.right);
+        InorderTranversal(root.left);
+        System.out.print(root.data+"->");
+        InorderTranversal(root.right);
     }
 }
