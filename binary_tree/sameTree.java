1f108d005153263f15d82ced1a6252e604899829
Godswillkhojee <ardiumwing@gmail.com>
2026-04-28 19:25:41 +0530

    Same Tree

---
+++ binary_tree/sameTree.java
@@ -0,0 +1,22 @@
+package binary_tree;
+
+public class sameTree {
+    public static void main(String[] args) {
+        node root = new node(1);
+        root.left = new node(2);
+        root.right = new node(3);
+        root.right.left = new node(5);
+
+        node root1 = new node(1);
+        root1.left = new node(2);
+        root1.right = new node(5);
+        root1.right.left = new node(5);
+        System.out.println(isSameTree(root, root1));
+    }
+    public static boolean isSameTree(node p, node q) {
+        if(q == null && p == null) return true;
+        if(q == null || p == null) return false;
+
+        return (q.data == p.data ) && isSameTree(q.left ,p.left) && isSameTree(q.right , p.right);
+    }
+}
