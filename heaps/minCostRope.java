f329c7437e3fe4f201b45f8b1b9489bcae700993
Godswillkhojee <ardiumwing@gmail.com>
2026-05-28 20:54:31 +0530

    min cost rope

--- heaps/minCostRope.java
+++ heaps/minCostRope.java
@@ -19,7 +19,7 @@ public class minCostRope {
     }
 
     public static void main(String[] args) {
-        int[] arr = {7, 10, 4, 3, 20, 15};
+        int[] arr = {7, 10, 4, 3, 15};
         System.out.println(minCost(arr));
     }
 }
