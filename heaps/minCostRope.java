92ac4e771e91326784de8dab4dea8de8dd6d41ba
Godswillkhojee <ardiumwing@gmail.com>
2026-05-28 20:44:59 +0530

    minCostRope

---
+++ heaps/minCostRope.java
@@ -0,0 +1,25 @@
+package heaps;
+
+import java.util.PriorityQueue;
+
+public class minCostRope {
+    public static int minCost(int[] arr) {
+        // code here
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int sum = 0;
+        for(int ele:arr) pq.add(ele);
+        while(pq.size()>1)
+        {
+            int a = pq.poll();
+            int b = pq.poll();
+            pq.add(a+b);
+            sum += a+b;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 10, 4, 3, 20, 15};
+        System.out.println(minCost(arr));
+    }
+}
