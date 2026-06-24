88972a1db46fbb109b5a266d3139e36906e7f85e
Godswillkhojee <ardiumwing@gmail.com>
2026-06-25 01:09:23 +0530

    longes non repeating substring

---
+++ hash/map/longestNonRepeatingSubstring.java
@@ -0,0 +1,19 @@
+package hash.map;
+
+import java.util.HashMap;
+
+public class longestNonRepeatingSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int i = 0;
+        for(int j = 0; j < s.length();j++)
+        {
+            char ch = s.charAt(j);
+            if(map.containsKey(ch)) i = Math.max(i,map.get(ch)+1);
+            map.put(ch,j);
+            max = Math.max(max,j-i+1);
+        }
+        return max;
+    }
+}
