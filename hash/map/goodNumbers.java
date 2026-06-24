7169bfa81fa7de3bebf9216e835dbe3f26fd0a09
Godswillkhojee <ardiumwing@gmail.com>
2026-06-24 23:53:20 +0530

    good numbers

---
+++ hash/map/goodNumbers.java
@@ -0,0 +1,24 @@
+package hash.map;
+
+import java.util.HashMap;
+
+public class goodNumbers {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int ele: nums) map.put(ele, map.getOrDefault(ele,0)+1);
+        int sum = 0;
+        for(int ele: map.keySet())
+        {
+            int freq = map.get(ele);
+            if(freq > 1)
+            {
+                while(freq != 0)
+                {
+                    freq--;
+                    sum+=freq;
+                }
+            }
+        }
+        return sum;
+    }
+}
