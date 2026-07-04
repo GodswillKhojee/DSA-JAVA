ce27efebc8894b953c18fec874bfcb95e2f3295c
Godswillkhojee <ardiumwing@gmail.com>
2026-07-04 21:19:30 +0530

    fibo using DP

---
+++ DynamicProgramming/fibonacci.java
@@ -0,0 +1,16 @@
+package DynamicProgramming;
+
+public class fibonacci {
+    public int fibo(int n, int [] lookUp)
+    {
+        if(n <= 1) return n;
+        if(lookUp[n] != 0) return lookUp[n];
+        int ans = fibo(n-1,lookUp) + fibo(n-2,lookUp);
+        lookUp[n] = ans;
+        return ans;
+    }
+    public int fib(int n) {
+        int [] lookUp = new int[n+1];
+        return fibo(n,lookUp);
+    }
+}
