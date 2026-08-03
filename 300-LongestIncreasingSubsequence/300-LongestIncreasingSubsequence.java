8c02e8d9303c8505ecd480aac8f857c90f35248a
Godswill Khojee <ardiumwing@gmail.com>
2026-08-03 21:15:23 +0530

    Create 300-LongestIncreasingSubsequence

---
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -0,0 +1,18 @@
+// Last updated: 03/08/2026, 21:15:22
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int [] dp = new int[n];
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < n;i++)
+        {
+            for(int j = 0;j <= i-1;j++)
+            {
+                if(nums[j]< nums[i]) dp[i] = Math.max(dp[i],dp[j]);
+            }
+            dp[i] += 1;
+            max = Math.max(max,dp[i]);
+        }
+        return max;
+    }
+}
