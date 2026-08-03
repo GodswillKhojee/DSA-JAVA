51f25b923010960bd2ee4f094077652b5828432f
Godswill Khojee <ardiumwing@gmail.com>
2026-08-03 21:16:21 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/189-RotateArray.cpp
@@ -0,0 +1,24 @@
+// Last updated: 03/08/2026, 21:16:21
+class Solution {
+public:
+    void revpart(int i, int j, vector<int>& v)
+    {
+        while (i<=j)
+        {
+            int temp = v[i];
+            v[i] = v[j];
+            v[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) 
+    {
+        int n = nums.size();
+        if(k>n) k = k % n;
+        revpart(0, n - k - 1, nums);
+        revpart(n-k, n - 1, nums);
+        revpart(0, n - 1, nums);
+
+    }
+};
