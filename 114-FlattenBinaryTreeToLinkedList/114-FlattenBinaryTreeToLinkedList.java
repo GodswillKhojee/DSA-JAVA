09ae958fc8e93331a794263d40af7befc48f35ed
Godswill Khojee <ardiumwing@gmail.com>
2026-08-04 00:20:16 +0530

    Create 114-FlattenBinaryTreeToLinkedList

---
+++ 114-FlattenBinaryTreeToLinkedList/114-FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,27 @@
+// Last updated: 04/08/2026, 00:20:15
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
