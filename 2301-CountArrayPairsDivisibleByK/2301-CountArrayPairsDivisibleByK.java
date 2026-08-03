1525dc39954e588b8d919e71d01bed1599384815
Godswill Khojee <ardiumwing@gmail.com>
2026-08-03 21:13:39 +0530

    Create 2301-CountArrayPairsDivisibleByK

---
+++ 2301-CountArrayPairsDivisibleByK/2301-CountArrayPairsDivisibleByK.java
@@ -0,0 +1,30 @@
+// Last updated: 03/08/2026, 21:13:39
+class Solution {
+    public long countPairs(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        long ans = 0;
+
+        for (int num : nums) {
+            int g = gcd(num, k);
+
+            for (int prevGcd : map.keySet()) {
+                if ((long) g * prevGcd % k == 0) {
+                    ans += map.get(prevGcd);
+                }
+            }
+
+            map.put(g, map.getOrDefault(g, 0) + 1);
+        }
+
+        return ans;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
