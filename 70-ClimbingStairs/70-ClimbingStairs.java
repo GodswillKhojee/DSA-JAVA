fbe4321c9e0734d5171a09ef64e4c96089b5f600
Godswill Khojee <ardiumwing@gmail.com>
2026-08-04 00:20:52 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,15 @@
+// Last updated: 04/08/2026, 00:20:52
+class Solution {  
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+        int [] dp = new int [n+1];
+        Arrays.fill(dp,-1);
+        dp[1] = 1;
+        if(n>= 1) dp[2] = 2;
+        for(int i = 3; i <= n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
