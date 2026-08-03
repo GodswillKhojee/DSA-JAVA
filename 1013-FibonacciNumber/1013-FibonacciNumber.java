5a9f5bb4e2d4f1dd15b5d3915d5f444ea8a1a240
Godswill Khojee <ardiumwing@gmail.com>
2026-08-03 21:14:24 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,15 @@
+// Last updated: 03/08/2026, 21:14:24
+class Solution {
+    public int fibo(int n, int [] lookUp)
+    {
+        if(n <= 1) return n;
+        if(lookUp[n] != 0) return lookUp[n];
+        int ans = fibo(n-1,lookUp) + fibo(n-2,lookUp);
+        lookUp[n] = ans;
+        return ans;
+    }
+    public int fib(int n) {
+        int [] lookUp = new int[n+1];
+        return fibo(n,lookUp);
+    }
+}
