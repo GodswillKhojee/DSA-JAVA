57dfd34e630f3079cbfbb582e581de450b92cbb6
Godswill Khojee <ardiumwing@gmail.com>
2026-08-03 21:14:39 +0530

    Create 745-FindSmallestLetterGreaterThanTarget

---
+++ 745-FindSmallestLetterGreaterThanTarget/745-FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,10 @@
+// Last updated: 03/08/2026, 21:14:39
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        for(int i = 0; i < letters.length;i++)
+        {
+            if(letters[i] > target) return letters[i];
+        }
+        return letters[0];
+    }
+}
