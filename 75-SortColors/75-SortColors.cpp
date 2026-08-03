50adfbe6090a746eece1dde057426a96ee14c8ad
Godswill Khojee <ardiumwing@gmail.com>
2026-08-04 00:20:50 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.cpp
@@ -0,0 +1,32 @@
+// Last updated: 04/08/2026, 00:20:49
+class Solution {
+public:
+    void sortColors(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int l = 0;
+        int m = 0;
+        int h = n - 1;
+
+        while(m <= h)
+        {
+            if(nums[m] == 2)
+            {
+                nums[m] = nums[h];
+                nums[h] = 2;
+                h--;
+            }
+            else if(nums[m] == 0)
+            {
+                nums[m] = nums[l];
+                nums[l] = 0;
+                l++;
+                m++;
+            }
+            else
+            {
+                m++;
+            }
+        }    
+    }
+};
