6a1a59fc27eb37e35bcc2a296969022cda113e03
Godswill Khojee <ardiumwing@gmail.com>
2026-08-03 21:14:53 +0530

    Create 532-KDiffPairsInAnArray

---
+++ 532-KDiffPairsInAnArray/532-KDiffPairsInAnArray.java
@@ -0,0 +1,25 @@
+// Last updated: 03/08/2026, 21:14:53
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        if(k < 0) return 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int ele: nums) map.put(ele,map.getOrDefault(ele,0)+1);
+        
+        int pairs = 0;
+        if(k==0)
+        {
+            for(int ele: map.values())
+            {
+                if(ele>1)pairs++;
+            }
+        }
+        else
+        {
+            for(int ele:map.keySet())
+            {
+                if(map.containsKey(ele+k)) pairs++;
+            }
+        }
+        return pairs;
+    }
+}
