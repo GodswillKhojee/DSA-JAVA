b69614f7dec21022e58fc53812623ea4d6461b9c
Godswill Khojee <ardiumwing@gmail.com>
2026-08-03 21:15:29 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,18 @@
+// Last updated: 03/08/2026, 21:15:29
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        int n = nums.length;
+        for(int j = 0;j<n;j++)
+        {
+            if(nums[j] != 0)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+        
+    }
+}
