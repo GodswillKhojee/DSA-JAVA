c33abea8f7e15cfbc521fb375b98e229a16e6894
Godswill Khojee <ardiumwing@gmail.com>
2026-08-03 21:14:33 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,15 @@
+// Last updated: 03/08/2026, 21:14:33
+class Solution {
+    public int helper(int [] nums, int target, int lo, int hi )
+    {
+        if(lo>hi) return -1;
+        int mid = lo + (hi-lo)/2;
+        if(nums[mid] == target) return mid;
+        else if (nums[mid]>target) return helper(nums, target, lo, mid-1 );
+        else return helper(nums, target, mid +1 , hi );
+    }
+    public int search(int[] nums, int target) {
+
+        return helper(nums,target,0,nums.length-1);
+    }
+}
