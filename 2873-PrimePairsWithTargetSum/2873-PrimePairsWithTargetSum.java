1d2773f2aea7b1366d8d27640b4b35e11a762570
Godswill Khojee <ardiumwing@gmail.com>
2026-08-03 21:13:24 +0530

    Create 2873-PrimePairsWithTargetSum

---
+++ 2873-PrimePairsWithTargetSum/2873-PrimePairsWithTargetSum.java
@@ -0,0 +1,38 @@
+// Last updated: 03/08/2026, 21:13:24
+class Solution {
+    public List<List<Integer>> findPrimePairs(int n) {
+        List<List<Integer>> bhai = new ArrayList<>();
+        
+        boolean [] prime = new boolean[n+1];
+
+        Arrays.fill(prime,true);
+        prime[0] = false;
+        prime[1] = false;
+        for(int i = 2; i *i<= n;i++)
+        {
+            if(prime[i])
+            {
+                for(int j = i*i;j<=n;j+=i)
+                {
+                    prime[j] = false;
+                }
+            }
+        }
+        int i = 0; int j = n;
+        while(i<=j)
+        {
+            if(prime[i] && prime[j] )
+            {
+                if(i+j == n) bhai.add(Arrays.asList(i++,j--));
+                else if(i+j > n) j--;
+                else i++;
+            }
+            i++;j--;
+        }
+        // for(int i = 2; i <=n/2;i++)
+        // {
+        //     if(prime[i]&&prime[n-i]) bhai.add(Arrays.asList(i,n-i));
+        // }
+        return bhai;
+    }
+}
