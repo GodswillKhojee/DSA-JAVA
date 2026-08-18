650261926513d7193235e35444cb8e6ac6a6991d
Godswill Khojee <ardiumwing@gmail.com>
2026-08-18 16:38:40 +0530

    Create 860-LemonadeChange

---
+++ 860-LemonadeChange/version/java/860-LemonadeChange_20260818_163839.java
@@ -0,0 +1,34 @@
+// Last updated: 18/08/2026, 16:38:39
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for(int ele : bills)
+        {
+            if(ele == 5) five++;
+            else if(ele == 10) 
+            {
+                if(five > 0) 
+                {
+                    five--;
+                    ten++;
+                }
+                else return false;
+            }
+            else
+            {
+                if(five >0 && ten>0)
+                {
+                    ten--;
+                    five--;
+                }
+                else if(five >= 3)
+                {
+                    five-=3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+}
