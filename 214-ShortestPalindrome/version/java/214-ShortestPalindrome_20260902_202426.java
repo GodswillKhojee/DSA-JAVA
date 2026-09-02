beda3f5bede22437fe8a8abf7e3e2b02cc1d1bf4
Godswill Khojee <ardiumwing@gmail.com>
2026-09-02 20:24:27 +0530

    Create 214-ShortestPalindrome

---
+++ 214-ShortestPalindrome/version/java/214-ShortestPalindrome_20260902_202426.java
@@ -0,0 +1,19 @@
+// Last updated: 02/09/2026, 20:24:26
+class Solution {
+    public String shortestPalindrome(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+        String pat = s + '#' + rev;
+        int n = pat.length();
+        int[] lps = new int[n];
+        int i = 0; int j = 1;
+        while(j<n)
+        {
+            if(pat.charAt(i) == pat.charAt(j)) lps[j++] = ++i;
+            else if(i>0) i = lps[i-1];
+            else j++;
+        }
+        String rem = s.substring(lps[n-1]);
+        String add = new StringBuilder(rem).reverse().toString();
+        return add+s;
+    }
+}
