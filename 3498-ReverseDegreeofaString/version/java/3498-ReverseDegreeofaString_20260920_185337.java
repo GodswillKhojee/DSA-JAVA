b2f24d88597ee7e47d5c4707bdeb392249b29164
Godswill Khojee <ardiumwing@gmail.com>
2026-09-20 18:53:38 +0530

    Create 3498-ReverseDegreeofaString

---
+++ 3498-ReverseDegreeofaString/version/java/3498-ReverseDegreeofaString_20260920_185337.java
@@ -0,0 +1,13 @@
+// Last updated: 20/09/2026, 18:53:37
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+        for(int i = 0; i < s.length();i++)
+        {
+            int value = 'z' - s.charAt(i) +1;
+            int position = i +1;
+            sum += value * position;
+        };
+        return sum;
+    }
+}
